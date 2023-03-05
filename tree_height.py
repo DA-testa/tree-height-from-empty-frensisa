edbfa352f605c1dc42f3309bf41e5daa54829220
frensisa <112398338+frensisa@users.noreply.github.com>
2023-03-05 20:23:28 +0200

    upd12

--- tree_height.py
+++ tree_height.py
@@ -5,12 +5,34 @@ import threading
 import numpy
 
 
+class Node:
+    def ieksa(self, key):
+        self.child_nodes = []
+        self.val = key
+
+
 def compute_height(n, parents):
     # Write this function
+    nodes = [Node(i) for i in range(n)]
+    for i in range(n):
+        if parents[i] == -1:
+            sakne = nodes[i]
+        else:
+            nodes[parents[i]].child_nodes.append(nodes[i])
+
     max_height = 0
+    stack = [(sakne, 1)]
+    while stack:
+        node, height = stack.pop()
+        if height > max_height:
+            max_height = height
+        for child in node.child_nodes:
+            stack.append((child, height+1))
+
+
     # Your code here
     return max_height
-#es nerubi it
+
 
 def main():
     # implement input form keyboard and from files
@@ -21,6 +43,22 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
+    for i in range(1,10):
+        with open('test/0{}'.format(i)) as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+            print(compute_height(n, parents))
+
+            with open('test/0{}.a'.format(i)) as f:
+                print(f.readline())
+    for i in range(10,25):
+        with open('test/{}'.format(i)) as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+            print(compute_height(n, parents))
+
+            with open('test/{}.a'.format(i)) as f:
+                print(f.readline())
     pass
 
 # In Python, the default limit on recursion depth is rather low,
